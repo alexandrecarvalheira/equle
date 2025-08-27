6a12fbccadfcd7bcc0299701f41cbd581068c808
Alexandre Carvalheira <axxcar@gmail.com>
2025-08-27 17:31:31 -0300

    revert to wagmi provider

--- packages/cofhe-nextjs/src/app/providers.tsx
+++ packages/cofhe-nextjs/src/app/providers.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiProvider } from "wagmi";
+import { wagmiConfig } from "./wagmi.config";
 import { baseSepolia } from "wagmi/chains";
 import { ReactNode } from "react";
 import { MiniKitProvider } from "@coinbase/onchainkit/minikit";
@@ -8,21 +10,25 @@ const queryClient = new QueryClient();
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
-    <MiniKitProvider
-      chain={baseSepolia}
-      apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
-      config={{
-        appearance: {
-          name: "(Equle*)", // Displayed in modal header
-          mode: "dark", // 'light' | 'dark' | '400'
-          theme: "default", // 'default' or custom theme
-        },
-        wallet: {
-          display: "modal",
-        },
-      }}
-    >
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </MiniKitProvider>
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <MiniKitProvider
+          chain={baseSepolia}
+          apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
+          config={{
+            appearance: {
+              name: "(Equle*)", // Displayed in modal header
+              mode: "dark", // 'light' | 'dark' | '400'
+              theme: "default", // 'default' or custom theme
+            },
+            wallet: {
+              display: "modal",
+            },
+          }}
+        >
+          {children}
+        </MiniKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 }
