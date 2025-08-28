9a3ad32c23adf0732a71f884e94c70dd888abb2e
Alexandre Carvalheira <axxcar@gmail.com>
2025-08-28 11:11:22 -0300

    guess fn v1

--- packages/hardhat/contracts/Equale.sol
+++ packages/hardhat/contracts/Equale.sol
@@ -7,15 +7,18 @@ import "@fhenixprotocol/cofhe-contracts/FHE.sol";
 contract Equale {
     uint256 public immutable startTimestamp;
     uint256 public constant DAY = 1 days;
+    euint8 public ZERO;
+    euint8 public ONE;
+    euint8 public TWO;
 
     // mapping: gameid -> player -> guess (storing current attempt)
-    mapping(uint256 => mapping(address => uint256)) public gamePlayerGuess;
+    mapping(uint256 => mapping(address => euint8[5])) public PlayerGuess;
     
     // mapping gameid -> player -> won
-    mapping(uint256 => mapping(address => bool)) public gamePlayerWon;
+    mapping(uint256 => mapping(address => bool)) public PlayerWon;
     
     // mapping -> gameid -> result (target result as euint for privacy)
-    mapping(uint256 => euint32) public gameResult;
+    mapping(uint256 => euint16) public gameResult;
     
     // mapping -> gameid -> equation
     // [0] and [4] cant be operators(< 10)
@@ -32,6 +35,12 @@ contract Equale {
 
     constructor() {
         startTimestamp = block.timestamp;
+        ZERO = FHE.asEuint8(0);
+        ONE = FHE.asEuint8(1);
+        TWO = FHE.asEuint8(2);
+        FHE.allowThis(ZERO);
+        FHE.allowThis(ONE);
+        FHE.allowThis(TWO);
     }
 
     /**
@@ -41,4 +50,39 @@ contract Equale {
     function getCurrentGameId() public view returns (uint256) {
         return ((block.timestamp - startTimestamp) / DAY) + 1;
     }
+
+    function guess(InEuint8[5] calldata equationGuess,InEuint16 calldata resultGuess) public returns(euint8[5] memory EqAnswer, euint8 resultAnswer) {
+        uint256 gameId = getCurrentGameId();
+        euint16 result = FHE.asEuint16(resultGuess);
+        euint8[5] memory eqGuess;
+
+
+        //equation guess loop
+        // green = 0, orange = 1, grey = 2
+        for(uint256 i = 0; i < 5; i++){
+            eqGuess[i] = FHE.asEuint8(equationGuess[i]);
+
+            ebool isGreen = FHE.eq(eqGuess[i], gameEquation[gameId][i]);
+            ebool isOrange = _isOrange(eqGuess[i]);
+            EqAnswer[i] = FHE.select(isGreen, ZERO, FHE.select(isOrange, ONE, TWO));
+        }
+        //result compare
+        // lt = 1, eq = 0, gt = 2
+        ebool isResultCorrect = FHE.eq(result, gameResult[gameId]);
+        resultAnswer = FHE.select(isResultCorrect, ZERO, FHE.select(FHE.lt(result, gameResult[gameId]), ONE, TWO));
+    //how to check if the equation is correct? how to check if all tiles are green? maybe use zamas approach of modular ?
+    }
+
+
+
+    function _isOrange(euint8 tileGuess) private returns(ebool){
+        uint256 gameId = getCurrentGameId();
+        ebool found = FHE.eq(tileGuess, gameEquation[gameId][0]);
+        for(uint256 i = 1; i < 5; i++){
+            ebool equal = FHE.eq(tileGuess, gameEquation[gameId][i]);
+            found = FHE.or(found, equal);
+        }
+        return found;
+    }
 }
+
