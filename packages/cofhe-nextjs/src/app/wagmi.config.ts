a0aacb4716a552958946e75226844aac901cb148
Alexandre Carvalheira <axxcar@gmail.com>
2025-08-29 21:28:26 -0300

    miniappconnector try

--- packages/cofhe-nextjs/src/app/wagmi.config.ts
+++ packages/cofhe-nextjs/src/app/wagmi.config.ts
@@ -1,6 +1,7 @@
 import { baseSepolia, base } from "wagmi/chains";
 import { createConfig, http } from "wagmi";
 import { coinbaseWallet, injected } from "wagmi/connectors";
+import { farcasterMiniApp as miniAppConnector } from "@farcaster/miniapp-wagmi-connector";
 
 export const wagmiConfig = createConfig({
   chains: [baseSepolia, base],
@@ -9,6 +10,7 @@ export const wagmiConfig = createConfig({
       appName: "onchainkit",
       version: "4",
     }),
+    miniAppConnector(),
   ],
   ssr: true,
   // turn off injected provider discovery
